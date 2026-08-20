3570be790db303360538778bec72bf74e0748b9e
Jiten Nayak <jitanayak354@gmail.com>
2026-08-20 20:43:45 +0530

    Update 01-CheckEvenOdd.java

--- 01-Basic/01-CheckEvenOdd.java
+++ 01-Basic/01-CheckEvenOdd.java
@@ -1,12 +1,11 @@
-public class CheckEvenOdd {
+public class EvenOdd {
 
     public static boolean isEven(int n) {
         return n % 2 == 0;
     }
 
     public static void main(String[] args) {
-
-        int n = 15;
+        int n = 19;
 
         System.out.println(isEven(n));
     }
