52f12457266d369fe6bb9eacfc541b1efa784b60
Jiten Nayak <jitanayak354@gmail.com>
2026-08-20 20:48:15 +0530

    Update 03-SumOfNaturals.java

--- 01-Basic/03-SumOfNaturals.java
+++ 01-Basic/03-SumOfNaturals.java
@@ -1,20 +1,18 @@
-import java.io.*;
+public class SumOfNaturalNumbers {
 
-class GfG{
-    static int findSum(int n){
+    public static int sumNaturalNumbers(int n) {
         int sum = 0;
-        
-        // iterating over all the numbers 
-        // between 1 to n
-        for (int i= 1; i <= n; i++) 
-        {
-            sum = sum + i;
+
+        for (int i = 1; i <= n; i++) {
+            sum += i;
         }
+
         return sum;
     }
-    public static void main(String args[])
-    {
+
+    public static void main(String[] args) {
         int n = 5;
-        System.out.println(findSum(n));
-    } 
+
+        System.out.println(sumNaturalNumbers(n));
+    }
 }
