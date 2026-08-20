0167522fb27dc8dfbc77c0ed17ae27cb48aaedf0
Jiten Nayak <jitanayak354@gmail.com>
2026-08-20 08:45:44 +0530

    Add even odd program

---
+++ 01-Basics/02-Conditions/EvenOdd.java
@@ -0,0 +1,13 @@
+public class EvenOdd {
+
+    public static boolean isEven(int n) {
+        return n % 2 == 0;
+    }
+
+    public static void main(String[] args) {
+
+        int n = 15;
+
+        System.out.println(isEven(n));
+    }
+}
